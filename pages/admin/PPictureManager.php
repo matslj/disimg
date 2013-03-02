252f4f9dac36be48d552344356dc095a7156e284
Mats <mats@mats-amilo.(none)>
2013-03-02 13:44:43 +0100

    unspecified modification

--- pages/admin/PPictureManager.php
+++ pages/admin/PPictureManager.php
@@ -37,7 +37,6 @@ $htmlHead = <<<EOD
 
     <!-- jQuery Form Plugin -->
     <script type='text/javascript' src='{$js}jquery-form/jquery.form.js'></script>
-    <script type='text/javascript' src='{$js}jquery-twosidedmultiselector/jquery.twosidedmultiselector.js'></script>
 EOD;
     
 $redirectOnSuccess = 'json';
@@ -48,43 +47,7 @@ $javaScript = <<<EOD
 //
 (function($){
     $(document).ready(function() {
-        $(".multiselect").twosidedmultiselect();
         
-        // How to get the current selected items (note, they won't always have a "selected" attribute, but they will be in the box with the original ID:
-	// var selectedOptions = $("#yourselect")[0].options;
-        
-
-        var options = {
-            success:       showResponse,  // post-submit callback 
-            dataType:  "json"
-        }; 
-        // Bind to form
-        $('#form1').ajaxForm(options);
-
-        // post-submit callback 
-        function showResponse(data) {
-            $('#page_id').val(data.pageId);
-            $('p.notice').html("Saved: " + data.timestamp);
-            $('button#savenow').attr('disabled', 'disabled');
-        }
-
-        function getUserInfoFromRow(targetId) {
-            obj = {};
-            var indexDelimiter = targetId.indexOf("_");
-            var rowIndex = -1;
-            if (indexDelimiter > 0) {
-                rowIndex = targetId.substring(indexDelimiter + 1);
-                obj.accountid = $('#idUser_' + rowIndex).html();
-                obj.accountname = $('#accountName_' + rowIndex).html();
-                obj.name = $('#nameUser_' + rowIndex).html();
-                obj.email = $('#emailUser_' + rowIndex).html();
-             
-                return obj;
-            } else {
-                return null;
-            }
-        }
-
 	// ----------------------------------------------------------------------------------------------
 	//
 	// Event handler for buttons in form. Instead of messing up the html-code with javascript.
@@ -127,24 +90,11 @@ $uo = CUserData::getInstance();
 $account = $uo -> getAccount();
 $userId	= $uo -> getId();
 
-
-// $log -> debug("userid: " . $userId);
-// Always check whats coming in...
-//$pc->IsNumericOrDie($articleId, 0);
-
 // -------------------------------------------------------------------------------------------
 //
-// Get content of file archive from database
-//
-//$db 		= new CDatabaseController();
-//$mysqli = $db->Connect();
-//$attachment = new CAttachment();
-//$archiveDb = $attachment -> getFileList($db, $userId, $pc->computePage());
-//// $archiveDb = $attachment -> getDownloads($db, $userId, 'archive');
-//$mysqli->close();
-
-// -------------------------------------------------------------------------------------------
-//
+// First we're going to present the user with a list of users (in a drop down).
+// This choice will affect the list of folders presented next.
+// 
 // Reuse query from PUsersList.php
 //
 $httpRef = "";
@@ -194,7 +144,6 @@ $db->RetrieveAndStoreResultsFromMultiQuery($results);
 
 $htmlFolderList = <<< EOD
 <div id="folderList">
-    <p><a href="#" id="folder-link" class="dialog-link ui-state-default ui-corner-all create"><span class="ui-icon ui-icon-newwin create"></span>Skapa katalog</a></p>
     <table id="folders">
     <tr>
     <th><a href='{$httpRef}idFolder'>Id</a></th>
@@ -231,39 +180,10 @@ $mysqli->close();
 
 
 $action = "?p=" . $pc->computePage() . "p";
+$redirect = "?p=" . $pc->computePage();
 
 $htmlFolderList .= <<< EOD
     </table>
-</div>
-<!-- ui-dialog create -->
-<div id="dialogCreate" title="Dialog Title">
-    <form id='dialogCreateForm' action='{$action}' method='POST'>
-        <input type='hidden' name='redirect' value='{$redirect}'>
-        <input type='hidden' name='redirect-failure' value='{$redirect}'>
-        <input type='hidden' id='dialogCreateAction' name='action' value='create'>
-        <fieldset>
-            <p>Skapa ny katalog</p>
-            <table width='99%'>
-                <tr>
-                    <td><label for="dialogCreateFolderName">Namn: </label></td>
-                    <td style='text-align: right;'><input id='dialogCreateFolderName' class='name' type='text' name='foldername' value='' /></td>
-                </tr>
-            </table>
-        </fieldset>
-    </form>
-</div>
-<!-- ui-dialog delete -->
-<div id="dialogDelete" title="Radera katalog">
-    <form id='dialogDeleteForm' action='{$action}' method='POST'>
-        <input type='hidden' name='redirect' value='{$redirect}'>
-        <input type='hidden' name='redirect-failure' value='{$redirect}'>
-        <input type='hidden' id='dialogDeleteFolderId' name='folderid' value=''>
-        <input type='hidden' id='dialogDeleteAction' name='action' value='delete'>
-        <fieldset>
-            <p>Vill du radera den här katalogen?</p>
-            <div id="dialogDeleteFolderName"></div>
-        </fieldset>
-    </form>
 </div>
 EOD;
 
@@ -275,21 +195,7 @@ $htmlMain = <<<EOD
 <h1>File archive</h1>
 <div class='section'>
 {$selectOption}
-<p>You can use it on as many select lists as you wish and each will work independently.
-This list already has a couple of selected values.</p>
-<form method="post" action="">
-        <select name="yourselect" class="multiselect" size="6" multiple="true">
-        <option value="A" selected="true">The Letter A</option>
-        <option value="B">The Letter B</option>
-        <option value="C" selected="true">The Letter C</option>
-        <option value="D">The Letter D</option>
-        <option value="E" selected="true">The Letter E</option>
-        <option value="F">The Letter F</option>
-        </select>
-        <div style="clear: both;">
-                <input type="submit" value="Go">
-        </div>
-</form>
+{$htmlFolderList}
 </div>
 EOD;
 
